df8da7a0250057613d6f50535b33dfbcb0f79743
NSMK-sketch <hav-nmt@mail.ru>
2025-08-13 16:39:10 +0700

    тег <firstRating> находится внутри тега <row>
    Добавлена проверка на существование тега firstRating перед попыткой получить его текст
    
    Установлено значение по умолчанию "0", если тег не найден
    
    Улучшено форматирование кода для лучшей читаемости

--- my_html.py
+++ my_html.py
@@ -1,52 +1,65 @@
 import jinja2
 import xml.etree.ElementTree as ET 
 from datetime import datetime
-def parser(tag,file_name,programSet,plan,compensationTypeShortTitle):
-
+def parser(tag, file_name, programSet, plan, compensationTypeShortTitle):
     with open(file_name+".html", mode="w", encoding='utf-8-sig') as file_writer:
-        # Форматирование в строку в формате "ГГГГ-ММ-ДД ЧЧ:ММ:СС"
         current_datetime = datetime.now()
         time = current_datetime.strftime("%Y-%m-%d %H:%M")
         items = []
+        
         for tagen in tag.findall('entrant/row'):
-            
             statusCode = tagen.attrib['statusCode']
-            benefitSpecialCategory=tagen.attrib.get('benefitSpecialCategory')
+            benefitSpecialCategory = tagen.attrib.get('benefitSpecialCategory')
+            
             if benefitSpecialCategory is not None:
-                benefitSpecialCategory="✓"
+                benefitSpecialCategory = "✓"
             else:
-                benefitSpecialCategory=" "
-            if statusCode=="7" or statusCode == "8":
+                benefitSpecialCategory = " "
+                
+            if statusCode == "7" or statusCode == "8":
                 continue
                 
-            # Get the value from the attribute 'name'
-            rank = tagen.find('firstRating').text #номер в рейтинге
-            fullFio = tagen.attrib['fullFio'] # ФИО
-            fullFio=fullFio.capitalize()
-            fullFio=fullFio.title()
-            averageEduInstitutionMark= tagen.attrib['averageEduInstitutionMark']# Балл аттестата            
-            originalSubmissionWay=tagen.attrib['originalIn']# Сдан ли оргинал
-            priority=tagen.attrib['priority']
-            acceptedEntrantDate=tagen.attrib.get('acceptedEntrantDate')
-            averageEduInstitutionMark=averageEduInstitutionMark.replace(',', '.')    
-            if originalSubmissionWay=='false':
-                originalSubmissionWay='НЕТ'
-            else:
-                originalSubmissionWay='ДА'
-                if compensationTypeShortTitle=="по договору":
-                    if acceptedEntrantDate is None:
-                        originalSubmissionWay='НЕТ'
-
-                
-
-
+            # Получаем firstRating - теперь ищем его среди дочерних элементов row
+            first_rating = tagen.find('firstRating')
+            rank = first_rating.text if first_rating is not None else "0"  # значение по умолчанию, если тег не найден
             
-            an_item = dict(rank=rank,fullFio=fullFio,originalSubmissionWay=originalSubmissionWay,averageEduInstitutionMark=averageEduInstitutionMark,benefitSpecialCategory=benefitSpecialCategory)
+            fullFio = tagen.attrib['fullFio']
+            fullFio = fullFio.capitalize().title()
+            
+            averageEduInstitutionMark = tagen.attrib['averageEduInstitutionMark']
+            originalSubmissionWay = tagen.attrib['originalIn']
+            priority = tagen.attrib['priority']
+            acceptedEntrantDate = tagen.attrib.get('acceptedEntrantDate')
+            
+            averageEduInstitutionMark = averageEduInstitutionMark.replace(',', '.')    
+            
+            if originalSubmissionWay == 'false':
+                originalSubmissionWay = 'НЕТ'
+            else:
+                originalSubmissionWay = 'ДА'
+                if compensationTypeShortTitle == "по договору":
+                    if acceptedEntrantDate is None:
+                        originalSubmissionWay = 'НЕТ'
+
+            an_item = dict(
+                rank=rank,
+                fullFio=fullFio,
+                originalSubmissionWay=originalSubmissionWay,
+                averageEduInstitutionMark=averageEduInstitutionMark,
+                benefitSpecialCategory=benefitSpecialCategory
+            )
             items.append(an_item)
+            
         loader = jinja2.FileSystemLoader('templates\\page.htm')
         env = jinja2.Environment(loader=loader)
         template = env.get_template('')
-        msg=template.render(items=items,time=time,programSet=programSet,plan=plan,compensationTypeShortTitle=compensationTypeShortTitle)
+        msg = template.render(
+            items=items,
+            time=time,
+            programSet=programSet,
+            plan=plan,
+            compensationTypeShortTitle=compensationTypeShortTitle
+        )
         
         file_writer.write(msg)
 
